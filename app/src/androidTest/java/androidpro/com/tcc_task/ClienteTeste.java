1eb13408b77d96a2d2f05921ce3dd806fe653f2d
Mateuskrpi <802.173@alunos.unigran.br>
2021-06-01 19:26:19 -0400

    commit
    Alteração no teste realizada

--- app/src/androidTest/java/androidpro/com/tcc_task/ClienteTeste.java
+++ app/src/androidTest/java/androidpro/com/tcc_task/ClienteTeste.java
@@ -2,13 +2,17 @@ package androidpro.com.tcc_task;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 import androidpro.com.tcc_task.models.Cliente;
 
 public class ClienteTeste extends TestCase {
-    public void testClienteComIdadeMenorQue18NaoPodeTrabalhar(){
+    public void testClienteComIdadeMenorQue18NaoPodeAcessar(){
         Cliente c = new Cliente("Mateus",10);
         boolean resultado = c.idadeCliente();
         assertFalse(resultado);
 
     }
 }
+
+
--- app/src/androidTest/java/androidpro/com/tcc_task/PrestadorTest.java
+++ app/src/androidTest/java/androidpro/com/tcc_task/PrestadorTest.java
@@ -2,12 +2,22 @@ package androidpro.com.tcc_task;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 import androidpro.com.tcc_task.models.Prestador;
 
 public class PrestadorTest extends TestCase {
-    public void testePrestadorComIdadeMenorQue18anosNaoPodeVotar(){
+    public void testePrestadorComIdadeMenorQue18anosNaoTrabalha(){
         Prestador p = new Prestador("Felipe", 10);
         boolean resultado = p.idadePrestador();
         assertFalse(resultado);
     }
+    @Test
+    public void testVerificaIdade(){
+        assertEquals(21, d.verificaData(20,1999));
+    }
 }
+
+
+
+
